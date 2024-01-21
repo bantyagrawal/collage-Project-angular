b85ac059f5d6ab556c96f7230080a9217a4ea123
RajKumar2699 <singhhoney77824@gmail.com>
2024-01-21 12:07:44 +0530

    update

--- Collage-Project/src/app/app-routing.module.ts
+++ Collage-Project/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { OtpComponent } from './otp/otp.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path:'', component: LoginComponent},
+  {path:'', component: HomeComponent},
+  {path:'login', component: LoginComponent},
   {path:'otp', component: OtpComponent}
 ];
 
